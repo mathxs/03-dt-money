06cd304edfc410d1cfd07ee8c76b08ead7123aa9
mathxs <matheusdsantosp@gmail.com>
2024-10-10 15:52:10 -0300

    alguns ajustes de acessibilidade

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,30 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import logo from '../../assets/logo.svg';
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
+
+import logoImg from '../../assets/logo.svg'
 
 export function Header() {
   return (
-    <div>
-      <HeaderContainer>
-        <HeaderContent>
-          <img src={logo} alt="" />
-          
-          <NewTransactionButton>
-            Nova transação
-          </NewTransactionButton>
-        </HeaderContent>
-      </HeaderContainer>
-    </div>
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
   )
 }
