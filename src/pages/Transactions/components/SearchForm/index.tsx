7b7a49e93d4910b11264ab10cdc8e558e2e1afb6
mathxs <matheusdsantosp@gmail.com>
2024-10-11 15:39:43 -0300

    fim do modulo 3

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,14 +3,19 @@ import { useForm } from 'react-hook-form'
 import { SearchFormContainer } from './styles'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useContext } from 'react'
+import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 const searchFormSchema = z.object({
   query: z.string(),
 })
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionsContext)
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
 
   const {
     register,
