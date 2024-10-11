f7045dadb609f508220ce26c52690aaaac9af967
mathxs <matheusdsantosp@gmail.com>
2024-10-11 10:32:27 -0300

    req api e json

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 import { SearchForm } from './components/SearchForm'
@@ -7,7 +8,27 @@ import {
   TransactionsTable,
 } from './styles'
 
+interface Transaction {
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  category: string
+  createdAt: string
+}
+
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3333/transactions')
+    const data = await response.json()
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+
   return (
     <div>
       <Header />
@@ -18,41 +39,20 @@ export function Transactions() {
 
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de website</td>
-              <td>
-                <PriceHighlight variant="income">R$ 12.000</PriceHighlight>
-              </td>
-              <td> Venda </td>
-              <td>15/03/2021</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Desenvolvimento de website</td>
-              <td>
-                <PriceHighlight variant="outcome">-R$ 12.000</PriceHighlight>
-              </td>
-              <td> Venda </td>
-              <td>15/03/2021</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Desenvolvimento de website</td>
-              <td>
-                <PriceHighlight variant="income">R$ 12.000</PriceHighlight>
-              </td>
-              <td> Venda </td>
-              <td>15/03/2021</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Desenvolvimento de website</td>
-              <td>
-                <PriceHighlight variant="outcome">-R$ 12.000</PriceHighlight>
-              </td>
-              <td> Venda </td>
-              <td>15/03/2021</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              )
+            })}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
